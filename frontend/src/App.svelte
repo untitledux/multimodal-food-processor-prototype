934528d7308d7273c641dbe64c05d36b12389b73
Andreas Riedel <andreas.riedel95@gmail.com>
2021-04-10 11:55:02 +0200

    add basic node server

---
+++ frontend/src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  let streaming = false;
+
+  const stream = () => {
+    if (streaming) {
+    } else {
+    }
+
+    streaming = !streamin;
+  };
+</script>
+
+<div>
+  <div>
+    <button on:click={stream} class="stream-btn">Record</button>
+  </div>
+</div>
+
+<style lang="scss">
+</style>
