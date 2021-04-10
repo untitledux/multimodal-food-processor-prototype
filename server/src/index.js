619fee9635edbec8362619951752c2081eec60fc
Andreas Riedel <andreas.riedel95@gmail.com>
2021-04-10 13:07:53 +0200

    remove gitignore files

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,43 @@
-import 'dotenv/config';
-import cors from 'cors';
-import express from 'express';
- 
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
 const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+const ss = require("socket.io-stream");
+
+const PORT = process.env.PORT;
+const HOST = "127.0.0.1";
+
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+io.on("connection", (client) => {
+  console.log("client connected");
+  client.on("disconnect", () => {
+    console.log("client disconnected");
   });
- 
-app.listen(process.env.PORT, () =>
-  console.log('Example app listening on port 3000!'),
+  client.emit("server_setup", `Server connected [id=${client.id}]`);
+
+  // when the client sends 'stream' events
+  ss(client).on("stream", (stream, data) => {
+    // get the name of the stream
+    const filename = path.basename(data.name);
+    // pipe the filename to the stream
+    stream.pipe(fs.createWriteStream(filename));
+    // make a detectIntStream call
+
+    // detectIntentStream(stream, function (results) {
+    //   console.log(results);
+    //   client.emit("results", results);
+    // });
+  });
+});
+
+server.listen(HOST, PORT, () =>
+  console.log(`Example app listening on port ${PORT}!`)
 );
