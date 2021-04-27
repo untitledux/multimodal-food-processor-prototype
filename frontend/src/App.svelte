9024e813ad0432389faed882b0de8bf76a3c5b54
Andreas Riedel <andreas.riedel95@gmail.com>
2021-04-27 13:55:56 +0200

    add exported code

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -84,52 +84,52 @@
   };
 
   const streamer = async () => {
-    if (streaming) return;
-    try {
-      console.log('STREAMING');
-      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-      streaming = true;
-      recordAudio = RecordRTC(stream, {
-        type: 'audio',
-        mimeType: 'audio/wav',
-        bufferSize: 16384,
-        sampleRate: 44100, //should be the same then in the server code
-        desiredSampRate: 16000,
-        recorderType: RecordRTC.StereoAudioRecorder,
-        numberOfAudioChannels: 1,
+    if (!streaming) {
+      try {
+        console.log('STREAMING');
+        const stream = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+        });
+        streaming = true;
+        recordAudio = RecordRTC(stream, {
+          type: 'audio',
+          mimeType: 'audio/wav',
+          bufferSize: 16384,
+          sampleRate: 44100, //should be the same then in the server code
+          desiredSampRate: 16000,
+          recorderType: RecordRTC.StereoAudioRecorder,
+          numberOfAudioChannels: 1,
 
-        // get intervals based blobs
-        // value in milliseconds
-        // as you might not want to make detect calls every seconds
-        timeSlice: 0,
-        ondataavailable: (blob) => {
-          mediaStream = stream;
-          // making use of socket.io-stream for bi-directional
-          // streaming, create a stream
-          let streamSS = ss.createStream();
-          // stream directly to server
-          // it will be temp. stored locally
-          console.log(blob);
-          ss(socket).emit('stream', streamSS, {
-            name: 'stream.wav',
-            socketId,
-            size: blob.size,
-          });
-          // pipe the audio blob to the read stream
-          ss.createBlobReadStream(blob).pipe(streamSS);
-        },
-      });
-      recordAudio.startRecording();
-    } catch (err) {
-      console.error(err);
+          // get intervals based blobs
+          // value in milliseconds
+          // as you might not want to make detect calls every seconds
+          timeSlice: 0,
+          ondataavailable: (blob) => {
+            mediaStream = stream;
+            // making use of socket.io-stream for bi-directional
+            // streaming, create a stream
+            let streamSS = ss.createStream();
+            // stream directly to server
+            // it will be temp. stored locally
+            console.log(blob);
+            ss(socket).emit('stream', streamSS, {
+              name: 'stream.wav',
+              socketId,
+              size: blob.size,
+            });
+            // pipe the audio blob to the read stream
+            ss.createBlobReadStream(blob).pipe(streamSS);
+          },
+        });
+        recordAudio.startRecording();
+      } catch (err) {
+        console.error(err);
+      }
+    } else {
+      await recordAudio.stopRecording();
+      streaming = false;
+      mediaStream.stop();
     }
-  };
-
-  const stopStreamer = async () => {
-    if (!streaming) return;
-    await recordAudio.stopRecording();
-    streaming = false;
-    mediaStream.stop();
   };
 </script>
 
@@ -140,8 +140,20 @@
 
 <div>
   <div>
-    <button on:click={streamer} class="stream-btn">Record</button>
-    <button on:click={stopStreamer} class="stream-btn">Stop</button>
+    <div
+      class:active={streaming ? true : false}
+      class="button"
+      on:click={streamer}
+    >
+      <div class="bar" />
+      <div class="bar" />
+      <div class="bar" />
+      <div class="bar" />
+      <div class="bar" />
+      <div class="bar" />
+      <div class="bar" />
+    </div>
+
     <div style="margin-top: 20px">
       Step: {step}
     </div>
@@ -149,4 +161,66 @@
 </div>
 
 <style lang="scss">
+  .button {
+    height: 100px;
+    display: flex;
+    cursor: pointer;
+    justify-content: center;
+    align-items: center;
+    width: 100px;
+    background-color: red;
+    border-radius: 50%;
+    &.active > .bar {
+      animation: sound 0ms -600ms linear infinite alternate;
+    }
+  }
+
+  .bar {
+    background: blue;
+    bottom: 1px;
+    height: 3px;
+    width: 5px;
+    margin: 0px 2px;
+    border-radius: 5px;
+  }
+
+  @keyframes sound {
+    0% {
+      opacity: 0.35;
+      height: 3px;
+    }
+    100% {
+      opacity: 1;
+      height: 50px;
+    }
+  }
+
+  .bar:nth-child(1) {
+    height: 10px;
+    animation-duration: 474ms !important;
+  }
+  .bar:nth-child(2) {
+    height: 30px;
+    animation-duration: 433ms !important;
+  }
+  .bar:nth-child(3) {
+    height: 50px;
+    animation-duration: 407ms !important;
+  }
+  .bar:nth-child(4) {
+    height: 70px;
+    animation-duration: 458ms !important;
+  }
+  .bar:nth-child(5) {
+    height: 50px;
+    animation-duration: 400ms !important;
+  }
+  .bar:nth-child(6) {
+    height: 30px;
+    animation-duration: 427ms !important;
+  }
+  .bar:nth-child(7) {
+    height: 10px;
+    animation-duration: 441ms !important;
+  }
 </style>
